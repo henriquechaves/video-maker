52f59415a84566b4bb0cf681a76abd70577c991b
henriquechaves <henrique.chaves@queimadiaria.com.br>
2019-02-26 18:06:49 -0300

    Evolução do index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const readline = require('readline-sync')
+
 function start() {
   const content = { }
   content.searchTerm = askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
 
-  function askAndReturnSearchTerm ( ) {
-      return 'TERM EXEMPLE'
+  function askAndReturnSearchTerm () {
+      return readline.question('Type a wikipedia search term: ')
   }
+
+  function askAndReturnPrefix () {
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+    const selectedPrefixtext = prefixes[selectedPrefixIndex]
+  }
+
   console.log(content)
     
 }
+
 start()
