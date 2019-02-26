027a2ca44d5c572f96b790fc583cbe8f6780ede4
henriquechaves <henrique.chaves@queimadiaria.com.br>
2019-02-26 08:53:43 -0300

    Criando a primeira função

---
+++ index.js
@@ -0,0 +1,11 @@
+function start() {
+  const content = { }
+  content.searchTerm = askAndReturnSearchTerm()
+
+  function askAndReturnSearchTerm ( ) {
+      return 'TERM EXEMPLE'
+  }
+  console.log(content)
+    
+}
+start()
